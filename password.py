1081c935c4cebb7a9e8f871153cdc94915668020
sortovaritu <sortovaritu@gmail.com>
2018-04-13 16:19:45 +0300

    Delete password.py

--- password.py
+++
@@ -1,7 +0,0 @@
-import string
-import random
-
-def randompassword(size):
-    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
-    return ''.join(random.choice(chars) for x in range(0,size))
-
